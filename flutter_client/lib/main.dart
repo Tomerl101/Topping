2039806c97a02959e3b302628efcdb0873896a67
Tomer <tomerl101@gmail.com>
2019-01-07 00:10:22 +0200

    create location list

--- flutter_client/lib/main.dart
+++ flutter_client/lib/main.dart
@@ -2,12 +2,23 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import './screens/locations.dart';
 
-class Locations extends StatelessWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: LocationCards,
+    return MaterialApp(
+      title: 'Locations',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: LocationsScreen(),
+        ),
+      ),
     );
   }
 }
